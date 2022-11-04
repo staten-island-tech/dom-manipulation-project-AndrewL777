c01f833a1c01afb8e5ffbce8975f7171b4aa3a75
AndrewL777 <7landrew7@gmail.com>
2022-11-04 13:56:25 -0400

    11-4-22

--- style.js
+++ style.js
@@ -1,4 +1,17 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
+  box: document.getElementById("big-black-box"),
+  text: document.querySelector("#text"),
+  points: document.querySelectorAll(".point"),
 };
 console.log(DOMSelectors.button);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContext = "This is now a big bigger red box";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
