0e84bf6d4a9b7949458ac4e68f27ebafb5330ce3
AndrewL777 <7landrew7@gmail.com>
2022-11-17 14:00:35 -0500

    232131

--- style.js
+++ style.js
@@ -1,23 +1,29 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   artist: document.getElementById("artist"),
-  title: document.querySelector(`tile`),
-  form: document.querySelector(`form`),
-  display: document.querySelector(`display`),
-  url: document.querySelector(`url`),
+  title: document.getElementById("tile"),
+  form: document.getElementById("form"),
+  display: document.getElementById("display"),
+  url: document.getElementById("url"),
 };
-
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  createcard();
-});
-function createcard() {
+function card() {
+  url = DOMSelectors.urlInput.value;
+  artist = DOMSelectors.artistInput.value;
+  title = DOMSelectors.titleInput.value;
   DOMSelectors.display.insertAdjacentHTML(
     "afterend",
     `
-    <div class='display-card'> 
-    <img class'display-img" src='${DOMSelectors.url.value}" /><img>
-    <h2 class
+    <div class="display-card"> 
+    <img class"display-img" src="${DOMSelectors.url.value}" /><img>
+    <h2 class="display-artist">${DOMSelectors.artist.value}</h2>
+    <h3 class="display-title">${DOMSelectors.title.value}</h3>
+    <button class="remove btn">Remove</button>
+    </div>
     `
   );
-} // this function will cause new HTML to be added when the input button is clicked.
+}
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  card();
+});
